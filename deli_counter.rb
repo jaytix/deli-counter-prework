15e998749a4a451a102a06804ef05379e1321e86
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-16 19:37:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,12 @@
 def line(line)
-  str = "The line is currently: "
+  str = "The line is currently:"
   if line.count == 0
     puts "The line is currently empty."
   else
-    line.each_with_index do |name, num|
+    num = 1
+    line.each do |name|
       str += " #{num}. #{name}"
+      num +=1
     end
     puts str
   end
